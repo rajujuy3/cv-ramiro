e8dc99c93cd133dd1a27c3b55039e13b151653d6
rajujuy3 <123751094+rajujuy3@users.noreply.github.com>
2023-02-25 11:37:53 -0300

    archivos finales
    Es la primera vez que subo esperemos que esten bien

---
+++ script.js
@@ -0,0 +1,37 @@
+function seleccion1() {
+    const mostrartitulo = document.querySelector(".titulo")
+   
+
+
+
+    mostrartitulo.innerHTML =  "Educacion media: Tecnico electromecanico,Superior: carrera de ing en sistemas no finalizada, Unju. Cursos de programacion: Javascript, CSS, HTML, idiomas: español nativo, ingles basico.";
+
+}
+function seleccion2() {
+    const mostrartitulo = document.querySelector(".titulo")
+   
+
+
+
+    mostrartitulo.innerHTML =  "Conocimientos avanzados en servidores informaticos, desarrollo web en formacion, Virtualizacion de equipos, manejo medio de base de datos";
+
+}
+function seleccion3() {
+    const mostrartitulo = document.querySelector(".titulo")
+   
+
+
+
+    mostrartitulo.innerHTML =  "Trabaje enseñando matematicas, luego como Sys Admin";
+
+}
+function seleccion4() {
+    const mostrartitulo = document.querySelector(".titulo")
+   
+
+
+
+    mostrartitulo.innerHTML =  "4 Hijos y una excelente mujer a mi lado, me gusta compartir con ellos";
+
+}
+
